5f5ef55dd112053614075dab5b30aa9760f2accb
SausageManZ <42648988+SausageManZ@users.noreply.github.com>
2019-10-23 15:22:42 +0200

    Create PartyManagerTest.java

---
+++ src/test/java/no/uis/imagegame/PartyManagerTest.java
@@ -0,0 +1,17 @@
+package no.uis.imagegame;
+
+import org.junit.Test;
+
+public class PartyManagerTest {
+
+    @Test
+    public void createPartyTest() {
+
+    }
+
+    @Test
+    public void queuePlayersTest() {
+
+    }
+
+}
