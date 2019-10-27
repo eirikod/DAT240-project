6ecb26676d5104dd0ef1960f81460afa9debdbd7
MarkusAaJohannessen <markus.aarekol@outlook.com>
2019-10-27 13:17:24 +0100

    gameTest
    minor update

--- src/test/java/no/uis/imagegame/gameTest.java
+++ src/test/java/no/uis/imagegame/gameTest.java
@@ -21,10 +21,46 @@ public class gameTest {
 
     @Test
     public void getAllLabelsTest() {
-        
+
     }
 
+    @Test
+    public void playTest() {
 
+
+    }
+
+    @Test
+    public void nestRoundTest() {
+
+
+    }
+
+    @Test
+    public void chooseSegmentTest() {
+
+
+    }
+
+    @Test
+    public void checkAnswerTest() {
+
+
+    }
+
+    @Test
+    public void giveUpTest() {
+
+
+    }
+
+    @Test
+    public void getScoreTest() {
+
+    
+    }
+
+    
 
 
 
