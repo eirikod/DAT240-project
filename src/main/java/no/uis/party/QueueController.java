f81693a6806a265381f33997aa7b98dff25afeaf
SausageManZ <42648988+SausageManZ@users.noreply.github.com>
2019-10-30 14:57:39 +0100

    Cleaned up some unnecessary code on QueueController

--- src/main/java/no/uis/party/QueueController.java
+++ src/main/java/no/uis/party/QueueController.java
@@ -1,5 +1,6 @@
 package no.uis.party;
 
+import no.uis.backend_pseudo_game.tools.TickExecution;
 import no.uis.players.Player;
 import no.uis.websocket.SocketMessage;
 import no.uis.websocket.WebSocketEventListener;
@@ -28,7 +29,6 @@ public class QueueController {
     private static HashMap<String, Player> players = new HashMap<>();
     private static ArrayList<Player> playerList = new ArrayList<>();
 
-    /*
     QueueController() {
         TickExecution updater = new TickExecution(1000L, this::update);
         updater.execute();
@@ -44,7 +44,6 @@ public class QueueController {
         }
     }
 
-     */
     @Autowired
     private SimpMessageSendingOperations messagingTemplate;
 
@@ -62,26 +61,6 @@ public class QueueController {
             players.put(socketMessage.getSender(), player);
             partyManager.queueUpPlayer(player);
         }
-    }
-
-    @MessageMapping(destinaitionName + "/{roomId}/sendMessage/")
-    public void sendMessage(@DestinationVariable String roomId, @Payload SocketMessage socketMessage) {
-        String url = format("/channel/%s", roomId);
-        messagingTemplate.convertAndSend(url, socketMessage);
-    }
-
-    @MessageMapping(destinaitionName + "/{roomId}/addUser")
-    public void addUser(@DestinationVariable String roomId, @Payload SocketMessage socketMessage,
-                        SimpMessageHeaderAccessor headerAccessor) {
-        String currentRoomId = (String) headerAccessor.getSessionAttributes().put("room_id", roomId);
-        if (currentRoomId != null) {
-            SocketMessage leaveMessage = new SocketMessage();
-            leaveMessage.setType("LEAVE");
-            leaveMessage.setSender(socketMessage.getSender());
-            messagingTemplate.convertAndSend(format("/channel/%s", currentRoomId), leaveMessage);
-        }
-        headerAccessor.getSessionAttributes().put("username", socketMessage.getSender());
-        messagingTemplate.convertAndSend(format("/channel/%s", roomId), socketMessage);
     }
 
     @GetMapping("/party")
