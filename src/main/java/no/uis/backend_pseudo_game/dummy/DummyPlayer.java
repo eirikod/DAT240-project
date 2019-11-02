7ae9b1b9f7da3a94b8dce54ca331a81f25dff897
MarkusAaJohannessen <markus.aarekol@outlook.com>
2019-11-02 18:22:22 +0100

    Update DummyPlayer.java

--- src/main/java/no/uis/backend_pseudo_game/dummy/DummyPlayer.java
+++ src/main/java/no/uis/backend_pseudo_game/dummy/DummyPlayer.java
@@ -4,7 +4,7 @@ package no.uis.backend_pseudo_game.dummy;
  * Dummy class of the player. Use this to construct the game code in a similar fashion
  * to connecting real users to the game.
  */
-// TODO: Add more documentation
+
 // TODO: Create event handling (such as onFinishedGame, etc.)
 public class DummyPlayer {
     private Long id;
@@ -13,55 +13,100 @@ public class DummyPlayer {
     private PlayerStatus status;
     private int score;
 
+    /**
+     * Defines the various enums for PlayerType
+     */
     public enum PlayerType {
         GUESSER, PROPOSER
     }
 
+    /**
+     * Defines the various enums for PlayerStatus
+     */
     public enum PlayerStatus {
         WAITING, PLAYING, FINISHED
     }
 
+    /**
+     * Creating a random id for the player internally
+     */
     public DummyPlayer() {
-        // Creating a random ID for the player internally.
         // TODO: Remove this on release if we intend to generate IDs from Spring Boot
         this.id = (long)(Math.random() * Long.MAX_VALUE);
         this.username = "user_" + this.id;
     }
 
+    /**
+     * Creates DummyPlayer object
+     * @param username updates the username
+     * @param pType updates the PlayerType
+     */
     public DummyPlayer(String username, PlayerType pType) {
         this.setUsername(username);
         this.setPlayerType(pType);
         this.setPlayerStatus(PlayerStatus.WAITING);
     }
 
+    /**
+     * Getter for player id
+     * @return the specific player id
+     */
     public Long getId() {
         return id;
     }
 
+    /**
+     * Setter for player id
+     * @param updates the specific player id
+     */
     public void setId(Long id) {
         this.id = id;
     }
 
+    /**
+     * Getter for the players username
+     * @return the specific username
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * Set the players username
+     * @param get the specific username
+     */
     public void setUsername(String username) {
         this.username = username;
     }
 
+    /**
+     * Getter for the player enum type
+     * @return PlayerType GUESSER or PROPOSER
+     */
     public PlayerType getPlayerType() {
         return type;
     }
 
+    /**
+     * Set the player enum PlayerType
+     * @param PlayerType GUESSER or PROPOSER
+     */
     public void setPlayerType(PlayerType playerType) {
         this.type = playerType;
     }
 
+    /**
+     * Getter for the score
+     * @return the score for player
+     */
     public int getScore() {
         return score;
     }
 
+    /**
+     * Setter for the score
+     * @param score
+     */
     public void setScore(int score) {
         this.score = score;
     }
