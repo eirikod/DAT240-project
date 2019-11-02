3ba3aca35c8ce881bc5abe652415aceb7387d5f5
SausageManZ <42648988+SausageManZ@users.noreply.github.com>
2019-11-02 16:29:38 +0100

    Preventing player from queueing into game if already playing or waiting

--- src/main/java/no/uis/players/User.java
+++ src/main/java/no/uis/players/User.java
@@ -1,29 +1,45 @@
 package no.uis.players;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@Table(name = "Players")
 public class User {
-	public String name;
-	@Id
-	public long id;
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public long getId() {
-		return id;
-	}
-	public void setId(long id) {
-		this.id = id;
-	}
-	@Override
-	public String toString() {
-		return "User [name=" + name + ", id=" + id + "]";
-	}
-	
-	
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "score")
+    private int score;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public User() {  }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String toString() {
+        return "User [name=" + username + ", id=" + id + "]";
+    }
 }
